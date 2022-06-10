de1577ec0c6e287240eae094301a1ff4364a4dc1
yellodot <rubio.gregoria@gmail.com>
2022-06-10 17:16:15 +0200

    feat: display numbers of cities found and changed bg color

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,15 @@ function App() {
   const [searchValue, setSearchValue] = useState('');
   const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    axios
-    .get(`http://localhost:3001/api/cities?city=${searchValue}`)
-    .then((res) => {
-      setResults(res.data);
-    })
-  }, [searchValue])
+  const fetchCities = async () => {
+    const res = await axios
+    .get(`http://localhost:3001/api/cities?city=${searchValue}`);
+    setResults(res.data)
+  }
 
-  const dataMetropole = results.metropole;
-  const dataOutremer = results.outremer;
+  useEffect(() => {
+    fetchCities();
+  }, [searchValue]);
 
   const handleChange = (e) => {
     setSearchValue(e.target.value);
@@ -23,52 +22,101 @@ function App() {
 
   return (
     <div className="m-6">
-      <div className="container flex justify-center mx-auto">
-          <div className="flex border-2 rounded">
-              <button className="flex items-center justify-center px-4 border-r">
+      <div className="flex justify-center">
+          <div className="flex border-2 rounded w-full">
+              <button className="h-20 w-2/5 text-3xl font-bold flex items-center justify-center px-4 border-r bg-[#D2E5E9]">
                   Je recherche...
               </button>
               <input 
               value={searchValue}
               onChange={handleChange}
-              type="text" className="px-4 py-2 w-80" placeholder="ville, code postal..."/>
+              type="text" className="px-4 py-2 w-full" placeholder="ville, code postal..."/>
           </div>
       </div>
-      <div className="flex justify-center mt-20">
-        <div className="w-1/2 text-lg">
-        <p className='text-xl font-bold'>
-          Villes de métropole
-        </p>
-          <ul className='flex flex-col'>
-            {
-              dataMetropole &&
-              dataMetropole.map((city, key) =>
-                <li id={key}>
-                  {city.nomCommune} {city.codePostal}
-                </li>
-              )
+      <div className='flex gap-x-10 mt-20'>
+          <div className="text-lg bg-[#D2E5E9] w-full text-center rounded-md">
+            <p className='text-3xl font-bold p-6'>
+              Villes de métropole
+            </p>
+            <div>
+            { 
+              searchValue != "" ?
+              !results.metropole || results.metropole.length === 0 ?
+              <div className='flex bg-red-600 mx-4 p-4'>
+              {
+              `Aucune ville correspondant au texte saisi`
+              }
+              </div>
+              :
+              <div className='flex bg-green-600 mx-4 p-4'>
+              {
+              `${results.metropole.length} ville(s)correspondant au texte saisi`
+              }
+              </div>
+              :
+              ""
             }
-          </ul>
-        </div>
-        <div className="text-lg">
-        <p className='text-xl font-bold'>
-          Villes d'outre-mer
-        </p>
-          <ul className='flex flex-col'>
-            {
-              dataOutremer &&
-              dataOutremer.map((city, key) =>
-                <li id={key}>
-                  {city.nomCommune} {city.codePostal}
-                </li>
-              )
+            </div>
+              <ul className='grid grid-cols-2'>
+                {
+                  results.metropole && results.metropole.length > 0 ?
+                  results.metropole.map((city, key) =>
+                    <li className="flex justify-between bg-slate-500 p-2 m-4 text-zinc-50" id={key}>
+                      <p className='font-bold text-left'>
+                        {city.nomCommune}
+                      </p>
+                      <p className='opacity-30'>
+                        {city.codePostal}
+                      </p>
+                    </li>
+                  )
+                  :
+                  ''
+                }
+              </ul>
+          </div>
+          <div className="text-lg bg-[#D2E5E9] w-full text-center rounded-md">
+            <p className='text-3xl font-bold p-6'>
+              Villes d'outre-mer
+            </p>
+            { 
+              searchValue != "" ?
+              !results.outremer || results.outremer.length === 0 ?
+              <div className='flex bg-red-600 mx-4 p-4'>
+              {
+              `Aucune ville correspondant au texte saisi`
+              }
+              </div>
+              :
+              <div className='flex bg-green-600 mx-4 p-4'>
+              {
+              `${results.outremer.length} ville(s)correspondant au texte saisi`
+              }
+              </div>
+              :
+              ""
             }
-          </ul>
-        </div>
+              <ul className='grid grid-cols-2'>
+                {
+                  results.outremer && results.outremer.length > 0 ?
+                  results.outremer.map((city, key) =>
+                  <li className="flex justify-between bg-slate-500 p-2 m-4 text-zinc-50" id={key}>
+                  <p className='text-left font-bold'>
+                    {city.nomCommune}
+                  </p>
+                  <p className='opacity-30'>
+                    {city.codePostal}
+                  </p>
+                </li>
+                  )
+                  :
+                  ''
+                }
+              </ul>
+          </div>
       </div>
     </div>
   );
 }
-
 
 export default App;
